2ae8e63b63047345f9b4eecc04967c7458748c64
myrian22 <myrian.fuente.alu@santamaria-artesano.org>
2023-02-02 10:05:21 +0100

    Se ha creada la rama clase nueva

---
+++ src/NuevaClase.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class NuevaClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("En la rama. Se cra la clase nueva");
+    }
+    
+}
