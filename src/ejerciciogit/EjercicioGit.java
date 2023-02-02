40c6af452244b5a0eb04b8f4e94a90701986c44e
myrian22 <myrian.fuente.alu@santamaria-artesano.org>
2023-02-02 09:18:36 +0100

    Lineas del metodo main

---
+++ src/ejerciciogit/EjercicioGit.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author Myrian
+ */
+public class EjercicioGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hoy es día 2 de Febrero");
+        System.out.println("Son las 9:10");
+    }
+    
+}
